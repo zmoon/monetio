6f2ed7aee079a3856a3d2bbbd5a31c9b735ed933
zmoon <zmoon92@gmail.com>
2024-08-07 14:45:45 -0500

    More flexible is-CI

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,4 +5,4 @@ import pytest
 
 @pytest.fixture(scope="session")
 def is_ci():
-    return os.environ.get("CI", "false").lower() == "true"
+    return os.environ.get("CI", "false").lower() in {"true", "yes", "1", ""}
